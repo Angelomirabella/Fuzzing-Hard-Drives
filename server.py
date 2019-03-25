6fd7f45fff0c2266640948f02b715e2ffc9532fe
angelo <s249503@studenti.polito.it>
2019-03-25 11:50:47 +0100

    socket timeout

--- server.py
+++ server.py
@@ -18,7 +18,6 @@ def quit_handler(signum, frame):
 
 def work():
     i=1
-
     while True:
         sem.acquire()
         if stop:
@@ -41,28 +40,36 @@ def go():
     t.daemon = True
     t.start()
 
-
+    timeout=10 #seconds
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.bind(('',int(sys.argv[1])))
     s.listen(5000)
+    s.settimeout(15)
     print 'Waiting Connections...'
     i=1
 
     while True:
-        client_s, addr= s.accept()
+        try:
+            client_s, addr= s.accept()
        
-        print i
-        raw_data=client_s.recv(13)
-        ata_pass_through={'opcode' : int(ord(raw_data[0])) , 'protocol' :  int(ord(raw_data[1])) , 'flags' :  int(ord(raw_data[2])) , 'features' :  int(ord(raw_data[3]))
+#            print i
+            raw_data=client_s.recv(13)
+            ata_pass_through={'opcode' : int(ord(raw_data[0])) , 'protocol' :  int(ord(raw_data[1])) , 'flags' :  int(ord(raw_data[2])) , 'features' :  int(ord(raw_data[3]))
             , 'sector_count' :  int(ord(raw_data[4])) , 'lba_low' :  int(ord(raw_data[5])) , 'lba_mid' :  int(ord(raw_data[6])) , 'lba_high' :  int(ord(raw_data[7]))
             ,  'device' :  int(ord(raw_data[8])) , 'command' :  int(ord(raw_data[9])) , 'reserved' :  int(ord(raw_data[10])) , 'control' :  int(ord(raw_data[11]))}
-        i+=1
+            i+=1
         
-        queue.put(ata_pass_through)
-        sem.release()
+            queue.put(ata_pass_through)
+            sem.release()
 
-        if i >= 1345:
-            exit(0)
-
+ #           if i == 63:
+  #              for el in raw_data:
+   #                 print hex(ord(el)), 
+        
+        except socket.timeout:
+            print 'Timeout expired!'
+            stop=True
+            sem.release()
+            exit(0)    
 if __name__=='__main__':
     go()
